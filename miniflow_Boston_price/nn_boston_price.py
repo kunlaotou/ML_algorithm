d1e30392a7c54689fc4746db6fb3380b302311bd
kunlaotou <tangwenhua2010@126.com>
2017-07-02 20:38:53 +0800

    Update nn_boston_price.py

--- miniflow_Boston_price/nn_boston_price.py
+++ miniflow_Boston_price/nn_boston_price.py
@@ -1,283 +1,93 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul  2 08:47:07 2017
+Created on Sun Jul  2 08:46:16 2017
 
 @author: Administrator
 """
 
+"""
+Check out the new network architecture and dataset!
+
+Notice that the weights and biases are
+generated randomly.
+
+No need to change anything, but feel free to tweak
+to test your network, play around with the epochs, batch size, etc!
+"""
+
 import numpy as np
+from sklearn.datasets import load_boston
+from sklearn.utils import shuffle, resample
+from miniflow_Boston_price import *
+
+# Load data
+data = load_boston()
+
+X_ = data['data']   #(506,13)
+y_ = data['target'] #(506,)
+
+# Normalize data
+X_ = (X_ - np.mean(X_, axis=0)) / np.std(X_, axis=0)
+
+#获取数据集的列数，即特征个数，并作为W1的行数
+n_features = X_.shape[1] 
+n_hidden = 10
+
+W1_ = np.random.randn(n_features, n_hidden)  #(13,10) 横轴是特征数(输入个数)，纵轴是隐藏层节点数
+b1_ = np.zeros(n_hidden)     #有几个隐藏节点，b的大小就是几 这里b = 10 
+
+W2_ = np.random.randn(n_hidden, 1)
+b2_ = np.zeros(1)
 
 
-class Node(object):
-    """
-    Base class for nodes in the network.
+# Neural network
+X, y = Input(), Input()
+W1, b1 = Input(), Input()
+W2, b2 = Input(), Input()
 
-    Arguments:
+l1 = Linear(X, W1, b1)
+s1 = Sigmoid(l1)
+l2 = Linear(s1, W2, b2)
+cost = MSE(y, l2)
 
-        `inbound_nodes`: A list of nodes with edges into this node.
-    """
-    def __init__(self, inbound_nodes=[]):
-        """
-        Node's constructor (runs when the object is instantiated). Sets
-        properties that all nodes need.
-        """
-        # A list of nodes with edges into this node.
-        self.inbound_nodes = inbound_nodes
-        # The eventual value of this node. Set by running
-        # the forward() method.
-        self.value = None
-        # A list of nodes that this node outputs to.
-        self.outbound_nodes = []
-        # New property! Keys are the inputs to this node and
-        # their values are the partials of this node with
-        # respect to that input.
-        self.gradients = {}
-        # Sets this node as an outbound node for all of
-        # this node's inputs.
-        for node in inbound_nodes:
-            node.outbound_nodes.append(self)
+feed_dict = {
+    X: X_,
+    y: y_,
+    W1: W1_,
+    b1: b1_,
+    W2: W2_,
+    b2: b2_
+}
 
-    def forward(self):
-        """
-        Every node that uses this class as a base class will
-        need to define its own `forward` method.
-        """
-        raise NotImplementedError
+epochs = 10
+# Total number of examples
+m = X_.shape[0]
+batch_size = 11
+steps_per_epoch = m // batch_size  #浮点数除法，其结果进行四舍五入。
 
-    def backward(self):
-        """
-        Every node that uses this class as a base class will
-        need to define its own `backward` method.
-        """
-        raise NotImplementedError
+graph = topological_sort(feed_dict)
+trainables = [W1, b1, W2, b2]
 
+print("Total number of examples = {}".format(m))
 
-class Input(Node):
-    """
-    A generic input into the network.
-    """
-    def __init__(self):
-        # The base class constructor has to run to set all
-        # the properties here.
-        #
-        # The most important property on an Input is value.
-        # self.value is set during `topological_sort` later.
-        Node.__init__(self)
+# Step 4  重复第 1-3 步，直到出现收敛情况或者循环被其他机制暂停（即迭代次数）。
+for i in range(epochs):
+    loss = 0
+    for j in range(steps_per_epoch):
+        # Step 1 从总的数据集中随机抽样一批数据
+        # Randomly sample a batch of examples
+        X_batch, y_batch = resample(X_, y_, n_samples=batch_size)
 
-    def forward(self):
-        # Do nothing because nothing is calculated.
-        pass
+        # Reset value of X and y Inputs
+        X.value = X_batch
+        y.value = y_batch
 
-    def backward(self):
-        # An Input node has no inputs so the gradient (derivative)
-        # is zero.
-        # The key, `self`, is reference to this object.
-        self.gradients = {self: 0}
-        # Weights and bias may be inputs, so you need to sum
-        # the gradient from output gradients.
-        for n in self.outbound_nodes:
-            self.gradients[self] += n.gradients[self]
+        # Step 2 前向和后向运行网络，计算梯度（根据第 (1) 步的数据）。
+        forward_and_backward(graph)
 
-class Linear(Node):
-    """
-    Represents a node that performs a linear transform.
-    """
-    def __init__(self, X, W, b):
-        # The base class (Node) constructor. Weights and bias
-        # are treated like inbound nodes.
-        Node.__init__(self, [X, W, b])
+        # Step 3 应用梯度下降更新。
+        sgd_update(trainables)
 
-    def forward(self):
-        """
-        Performs the math behind a linear transform.
-        """
-        X = self.inbound_nodes[0].value
-        W = self.inbound_nodes[1].value
-        b = self.inbound_nodes[2].value
-        self.value = np.dot(X, W) + b
+        loss += graph[-1].value
 
-    def backward(self):
-        """
-        Calculates the gradient based on the output values.
-        
-        第一个L节点的输入X是(11,13)即11个样本，13个特征,梯度是(11,10) 隐藏层10个节点
-        第二个L节点的输入是(10,1),10表示隐藏层10个节点，sigmod后变成10个数；梯度是(11,1) 
-        W1(13,10)   w2(10,1) w3(11,1) 11表示样本个数
-        dot的时候要注意，input都要转置
-        
-        
-        grad_cost从cost节点传出的格式是(n,1)n表示样本个数,后来因为隐藏层节点为10个，所以变成变成(11,10)11是样本个数
-        """
-        # Initialize a partial for each of the inbound_nodes.
-        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
-        # Cycle through the outputs. The gradient will change depending
-        # on each output, so the gradients are summed over all outputs.
-        for n in self.outbound_nodes:
-            # Get the partial of the cost with respect to this node.
-            grad_cost = n.gradients[self]
-            # Set the partial of the loss with respect to this node's inputs.
-            #隐藏节点要横着放，因此W要转置
-            self.gradients[self.inbound_nodes[0]] += np.dot(grad_cost, self.inbound_nodes[1].value.T)
-            # Set the partial of the loss with respect to this node's weights.
-            print(self.gradients[self.inbound_nodes[0]].shape)
-            self.gradients[self.inbound_nodes[1]] += np.dot(self.inbound_nodes[0].value.T, grad_cost)
-            # Set the partial of the loss with respect to this node's bias.
-            self.gradients[self.inbound_nodes[2]] += np.sum(grad_cost, axis=0, keepdims=False)
-
-
-class Sigmoid(Node):
-    """
-    Represents a node that performs the sigmoid activation function.
-    """
-    def __init__(self, node):
-        # The base class constructor.
-        Node.__init__(self, [node])
-
-    def _sigmoid(self, x):
-        """
-        This method is separate from `forward` because it
-        will be used with `backward` as well.
-
-        `x`: A numpy array-like object.
-        """
-        return 1. / (1. + np.exp(-x))
-
-    def forward(self):
-        """
-        Perform the sigmoid function and set the value.
-        """
-        input_value = self.inbound_nodes[0].value
-        self.value = self._sigmoid(input_value)
-        
-
-    def backward(self):
-        """
-        Calculates the gradient using the derivative of
-        the sigmoid function.
-        """
-        # Initialize the gradients to 0.
-        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
-        # Sum the partial with respect to the input over all the outputs.
-        for n in self.outbound_nodes:
-            grad_cost = n.gradients[self]
-            sigmoid = self.value
-            self.gradients[self.inbound_nodes[0]] += sigmoid * (1 - sigmoid) * grad_cost
-
-
-class MSE(Node):
-    def __init__(self, y, a):
-        """
-        The mean squared error cost function.
-        Should be used as the last node for a network.
-        """
-        # Call the base class' constructor.
-        Node.__init__(self, [y, a])
-
-    def forward(self):
-        """
-        Calculates the mean squared error.
-        """
-        # NOTE: We reshape these to avoid possible matrix/vector broadcast
-        # errors.
-        #
-        # For example, if we subtract an array of shape (3,) from an array of shape
-        # (3,1) we get an array of shape(3,3) as the result when we want
-        # an array of shape (3,1) instead.
-        #
-        # Making both arrays (3,1) insures the result is (3,1) and does
-        # an elementwise subtraction as expected.
-        y = self.inbound_nodes[0].value.reshape(-1, 1)
-        a = self.inbound_nodes[1].value.reshape(-1, 1)
-
-        self.m = self.inbound_nodes[0].value.shape[0]
-        # Save the computed output for backward.
-        self.diff = y - a
-        self.value = np.mean(self.diff**2)
-
-    def backward(self):
-        """
-        Calculates the gradient of the cost.
-        """
-        self.gradients[self.inbound_nodes[0]] = (2 / self.m) * self.diff
-        self.gradients[self.inbound_nodes[1]] = (-2 / self.m) * self.diff
-
-
-def topological_sort(feed_dict):
-    """
-    Sort the nodes in topological order using Kahn's Algorithm.
-
-    `feed_dict`: A dictionary where the key is a `Input` Node and the value is the respective value feed to that Node.
-
-    Returns a list of sorted nodes.
-    """
-
-    input_nodes = [n for n in feed_dict.keys()]
-
-    G = {}
-    nodes = [n for n in input_nodes]
-    while len(nodes) > 0:
-        n = nodes.pop(0)
-        if n not in G:
-            G[n] = {'in': set(), 'out': set()}
-        for m in n.outbound_nodes:
-            if m not in G:
-                G[m] = {'in': set(), 'out': set()}
-            G[n]['out'].add(m)
-            G[m]['in'].add(n)
-            nodes.append(m)
-
-    L = []
-    S = set(input_nodes)
-    while len(S) > 0:
-        n = S.pop()
-
-        if isinstance(n, Input):
-            n.value = feed_dict[n]
-
-        L.append(n)
-        for m in n.outbound_nodes:
-            G[n]['out'].remove(m)
-            G[m]['in'].remove(n)
-            # if no other incoming edges add to S
-            if len(G[m]['in']) == 0:
-                S.add(m)
-    return L
-
-
-def forward_and_backward(graph):
-    """
-    Performs a forward pass and a backward pass through a list of sorted Nodes.
-
-    Arguments:
-
-        `graph`: The result of calling `topological_sort`.
-    """
-    # Forward pass
-    for n in graph:
-        n.forward()
-
-    # Backward pass
-    # see: https://docs.python.org/2.3/whatsnew/section-slices.html
-    for n in graph[::-1]:
-        n.backward()
-
-
-def sgd_update(trainables, learning_rate=1e-2):
-    """
-    Updates the value of each trainable with SGD.
-
-    Arguments:
-
-        `trainables`: A list of `Input` Nodes representing weights/biases.
-        `learning_rate`: The learning rate.
-    """
-    # TODO: update all the `trainables` with SGD
-    # You can access and assign the value of a trainable with `value` attribute.
-    # Example:
-    # for t in trainables:
-    #   t.value = your implementation here
-    for t in trainables:
-        t.value = t.value - learning_rate * t.gradients[t]
-    pass
-      
-    
+    print("Epoch: {}, Loss: {:.3f}".format(i+1, loss/steps_per_epoch))

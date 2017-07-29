78fd801bf227f413ce951abcfcd70981dd476c63
kunlaotou <tangwenhua2010@126.com>
2017-07-29 11:08:49 +0800

    Update Weight_Initialization.py

--- Weight Initialization/Weight_Initialization.py
+++ Weight Initialization/Weight_Initialization.py
@@ -1,1 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 29 10:36:28 2017
+
+@author: tangwenhua
+
+"""
+import tensorflow as tf
+import helper
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+print('Getting MNIST Dataset...')
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+print('Data Extracted.')
+
+print(mnist.train.images.shape)
+
+# Save the shapes of weights for each layer
+layer_1_weight_shape = (mnist.train.images.shape[1], 256)
+layer_2_weight_shape = (256, 128)
+layer_3_weight_shape = (128, mnist.train.labels.shape[1])
+
+
+#All Zeros or Ones
+all_zero_weights = [
+    tf.Variable(tf.zeros(layer_1_weight_shape)),
+    tf.Variable(tf.zeros(layer_2_weight_shape)),
+    tf.Variable(tf.zeros(layer_3_weight_shape))
+]
+
+all_one_weights = [
+    tf.Variable(tf.ones(layer_1_weight_shape)),
+    tf.Variable(tf.ones(layer_2_weight_shape)),
+    tf.Variable(tf.ones(layer_3_weight_shape))
+]
+
+helper.compare_init_weights(
+    mnist,
+    'All Zeros vs All Ones',
+    [
+        (all_zero_weights, 'All Zeros'),
+        (all_one_weights, 'All Ones')])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
